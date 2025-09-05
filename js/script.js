f68e79f3e4cd2f26777cd1b5b3a7f07285c2baab
Tocacreamy <zekobaharudinfirdaus@gmail.com>
2025-09-05 21:29:42 +0700

    link into porto

--- js/script.js
+++ js/script.js
@@ -10,18 +10,28 @@ const cards = [
     description:
       "Project PKM Permainan edukasi seksual untuk anak berbasis balok dan kartu",
     tag: "3D Model | Illustrasi",
+    link:`/`,
   },
   {
     title: "Veriface",
     description:
       "Project Gemastik pembuatan presensi otomatis dengan face recognition",
     tag: "UI/UX | Team management",
+    link:`/`,
   },
   {
     title: "Maxy CV scoring",
     description: "Project MSIB Pembuatan skor cv otomatis",
     tag: "Team management | Frontend",
+    link:`/`,
   },
+  {
+    title: "Nurohim Web Profile",
+    description: "Web Profile ",
+    tag: "UI/UX | Frontend",
+    link:`nurohim.netlify.app`,
+  },
+
 ];
 
 const cardContainer = document.getElementById("container-cards");
@@ -38,7 +48,7 @@ cards.forEach((card) => {
     
     <div class="button-card">
       <h4 class="text-tag">${card.tag}</h4>
-      <button>Lihat</button>
+      <a href="${card.link}" class="btn-card">Lihat Project</a>
     </div>
   `;
 
